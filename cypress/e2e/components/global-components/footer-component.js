36a1e61196fc00a29eadfdb348e64260093960b3
Ingrith Santamaria <ingrithsanta@gmail.com>
2024-02-12 21:03:26 -0500

    Footer Automation: Click and validation links virtual office, Product Summary

--- cypress/e2e/components/global-components/footer-component.js
+++ cypress/e2e/components/global-components/footer-component.js
@@ -2,33 +2,36 @@ class footer {
   elements = {
     getFooter: () => cy.get('[id="page-footer"]'),
     getCoworkingFooter: () => cy.get("div.items-start a").eq(0),
+    getMeetingEventsFooter: () => cy.get('a[href="https://yourstudio.staging.tishmanspeyer.com/studio-gather/"]').eq(1),
+    getVirtualOfficeFooter: () => cy.get('a[href="https://yourstudio.staging.tishmanspeyer.com/virtual-office-signup/"]').eq(1),
+    getOurCompanyFooter: () => cy.get('a[href="https://yourstudio.staging.tishmanspeyer.com/about-us/"]').eq(1),
+    getProductSummaryFooter: () => cy.get('a[href="https://yourstudio.staging.tishmanspeyer.com/products/"]')
   };
-  clickAndValidateCoworkingFooter = () => {
-    this.elements.getCoworkingFooter().should("be.visible").click();
-  };
-  openAndValidateNewWindowCoworking() {
-      cy.get("div.items-start a").eq(0).click();
-      
-      // Utiliza cy.window() para acceder a la nueva ventana
-      cy.window().its('location.href').then((newWindowUrl) => {
-        cy.log(`La nueva ventana se abrió en la URL: ${newWindowUrl}`);
-      });
-  
-      // Agrega validaciones para los elementos en la nueva ventana
-      cy.get('h3', { timeout: 15000 }).should('exist');
+  openAndValidateNewWindowCoworking = () => {
+      this.elements.getCoworkingFooter().click();
+      this.openNewWindow()
     }
-//  openAndValidateNewWindowCoworking() {
-//       cy.get("div.items-start a").eq(0).click();
-//       cy.window().then((newWindow) => {
-//             cy.url({log: false}).then((newWindowUrl) => {
-//                   cy.log(`La nueva ventana se abrió en la URL: ${newWindowUrl}`)
-//             })
-//         cy.wrap(newWindow.location.href).as('newWindowUrl');
-//         cy.wait(15000);
-            
-//         // Agrega validaciones para los elementos en la nueva ventana
-//        cy.get('id["properties-list"]').find('div.region').should('exist')
-//       });
-//     }
+    openAndValidateNewWindowMeetingEvents = () => {
+      this.elements.getMeetingEventsFooter().should("be.visible").click()
+      this.openNewWindow()
+    }
+    openAndValidateNewWindowVirtualOffice = () => {
+      this.elements.getMeetingEventsFooter().should("be.visible").click()
+      this.openNewWindow()
+    }
+    openAndValidateNewWindowOurCompany = () => {
+      this.elements.getOurCompanyFooter().should("be.visible").click()
+      this.openNewWindow()
+    }
+    openAndValidateNewWindowProductSummary = () => {
+      this.elements.getProductSummaryFooter().should("be.visible").click()
+      this.openNewWindow()
+    }
+    openNewWindow = () => {
+      cy.window().its('location.href').then((newWindowUrl) => {
+            cy.log(`La nueva ventana se abrió en la URL: ${newWindowUrl}`);
+          });
+          cy.get('h3', { timeout: 15000 }).should('exist');
+  }
 }
 module.exports = new footer();
--- cypress/e2e/pages/footer.cy.js
+++ cypress/e2e/pages/footer.cy.js
@@ -1,13 +1,26 @@
-import { clickAndValidateCoworkingFooter, openAndValidateNewWindowCoworking } from "../components/global-components/footer-component";
-describe('RENAME ME!', () => {
+import {
+  openAndValidateNewWindowCoworking,
+  openAndValidateNewWindowMeetingEvents,
+  openAndValidateNewWindowVirtualOffice,
+  openAndValidateNewWindowProductSummary
+} from "../components/global-components/footer-component";
+describe("RENAME ME!", () => {
   beforeEach(() => {
     //VISIT GOES HERE.
     cy.visit("/spaces?region=new-york");
     cy.wait(15000);
-    cy.acceptCookiesIfExists()
+    cy.acceptCookiesIfExists();
   });
-  it('RENAME ME!', () => {
-    openAndValidateNewWindowCoworking()
-    //clickAndValidateCoworkingFooter()
-  })
-})
+  it("User clicks on link coworking footer", () => {
+    openAndValidateNewWindowCoworking();
+  });
+  it("User clicks on link Meeting Events footer", () => {
+    openAndValidateNewWindowMeetingEvents();
+  });
+  it("User clicks on link Virtual Office Footer", () => {
+    openAndValidateNewWindowVirtualOffice();
+  });
+  it("User clicks on link Product summary Footer", () => {
+    openAndValidateNewWindowProductSummary();
+  });
+});
