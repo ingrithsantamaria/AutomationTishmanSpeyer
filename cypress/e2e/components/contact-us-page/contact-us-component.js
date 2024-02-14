f4d88f29388a6a33e16b3ea888dec4c7426d2772
Ingrith Santamaria <ingrithsanta@gmail.com>
2024-02-14 14:30:51 -0500

    Contact US Automation: Fill out form complete

--- cypress/e2e/components/contact-us-page/contact-us-component.js
+++ cypress/e2e/components/contact-us-page/contact-us-component.js
@@ -1,41 +1,110 @@
-import { faker } from '@faker-js/faker'
+import { faker } from "@faker-js/faker";
 class contact {
-    elements = {
-          getFirstNameField : () => cy.get('[id="firstName-field"]'),
-          getLastNameField : () => cy.get('[id="lastName-field"]'),
-          getEmailField : () => cy.get('[id="email-field"]'),
-          getPhoneNumberField: () => cy.get('[id="phone-field"]'),
-          getNavBarOption1: () => cy.contains('span', 'I have a general inquiry'),
-          getNavBarOption2: () => cy.get('button[data-tab="true"] span').eq(1)
+  elements = {
+    getFirstNameField: () => cy.get('[id="firstName-field"]'),
+    getLastNameField: () => cy.get('[id="lastName-field"]'),
+    getEmailField: () => cy.get('[id="email-field"]'),
+    getPhoneNumberField: () => cy.get('[id="phone-field"]'),
+    getNavBarOption1: () => cy.contains("span", "I have a general inquiry"),
+    getNavBarOption2: () => cy.get('button[data-tab="true"] span').eq(1),
+    getTextArea: () => cy.get("textarea#assistDetails-field"),
+    getCheckBoxReceiveMail: () => cy.get('button[role="switch"]'),
+    getListLocation: () => cy.get('button[aria-haspopup="listbox"]'),
+    getSubmit: () => cy.get('button[type="submit"]')
+  };
+  validateBodyContactUs = () => {
+    cy.get("body").contains("h1", "Contact Us");
+    cy.get("body").contains("h2", "Connect With Us");
+    const rightPanelElements = [
+      "Office Hours",
+      "Monday to Friday",
+      "9 a.m. to 5 p.m. ET",
+      "Address",
+      "66 Hudson Blvd E",
+      "23rd Floor",
+      "New York, NY 10001, USA",
+      "General Inquiries",
+      "info@yourstudio.com",
+      "212-523-0850",
+      "Brokers",
+      "brokers@yourstudio.com",
+    ];
+    rightPanelElements.forEach((value) => {
+      cy.get("p").should("contain", value);
+    });
+  };
+  fillContactUsInformation = () => {
+    this.elements
+      .getFirstNameField()
+      .should("be.visible")
+      .type(faker.person.firstName());
+    this.elements
+      .getLastNameField()
+      .should("be.visible")
+      .type(faker.person.lastName());
+    this.elements
+      .getEmailField()
+      .should("be.visible")
+      .type(faker.internet.email());
+    this.elements
+      .getPhoneNumberField()
+      .should("be.visible")
+      .type(faker.phone.number());
+    this.selectRamdonNavBar();
+    this.elements.getSubmit().should('be.visible').click({force: true})
+  };
+  selectRamdonNavBar = () => {
+    const randomOptionNavBar =
+      Math.random() < 0.5
+        ? this.elements.getNavBarOption1()
+        : this.elements.getNavBarOption2();
+    randomOptionNavBar.click();
+    if (randomOptionNavBar === this.elements.getNavBarOption1()) {
+      this.fillNavBarOption1Information();
+    } else {
+      this.fillNavBarOption2Information();
     }
-    validateBodyContactUs = () => {
-      cy.get('body').contains('h1', 'Contact Us')
-      cy.get('body').contains('h2', 'Connect With Us')
-      const rightPanelElements = [
-            'Office Hours',
-            'Monday to Friday',
-            '9 a.m. to 5 p.m. ET',
-            'Address',
-            '66 Hudson Blvd E',
-            '23rd Floor',
-            'New York, NY 10001, USA',
-            'General Inquiries',
-            'info@yourstudio.com',
-            '212-523-0850',
-            'Brokers',
-            'brokers@yourstudio.com'
-      ]
-      rightPanelElements.forEach((value) => {
-            cy.get('p').should('contain', value)
-      })
-    }
-    fillContactUsInformation = () => {
-      this.elements.getFirstNameField().should('be.visible').type(faker.person.firstName())
-      this.elements.getLastNameField().should('be.visible').type(faker.person.lastName())
-      this.elements.getEmailField().should('be.visible').type(faker.internet.email())
-      this.elements.getPhoneNumberField().should('be.visible').type(faker.phone.number())
-      const randomOptionNavBar = Math.random() < 0.5 ? this.elements.getNavBarOption1() : this.elements.getNavBarOption2()
-      randomOptionNavBar.click()
-    }
+  };
+  get optionsSelectLocation() {
+    return cy.get('ul').find('li[role=option]')
   }
-  module.exports = new contact();
+  randomOptionSelectLocation = () => {
+    this.elements.getListLocation().click();
+    this.optionsSelectLocation.then((optionsSelectLocation) => {
+      const randomOptions = Cypress._.sample(optionsSelectLocation);
+      randomOptions.click();
+    });
+  };
+  fillNavBarOption1Information = () => {
+    this.randomOptionSelectLocation();
+    this.elements
+      .getTextArea()
+      .should("be.visible")
+      .type(faker.lorem.paragraph(3, "<br/>\n"));
+    this.elements.getCheckBoxReceiveMail().should("be.visible").click();
+  };
+  fillNavBarOption2Information = () => {
+    this.randomOptionSelectLocation();
+    this.checkRandomCheckbox();
+    this.elements
+      .getTextArea()
+      .should("be.visible")
+      .type(faker.lorem.paragraph(3, "<br/>\n"));
+    this.elements.getCheckBoxReceiveMail().should("be.visible").click({multiple: true});
+  };
+  get checkBoxInterested () {
+      return cy.get('div').find('button[type=button]')
+  }
+  checkRandomCheckbox = () => {
+    this.checkBoxInterested
+      .then((parentCheckboxes) => {
+        const totalParentCheckboxes = parentCheckboxes.length;
+        const randomParentIndex = Cypress._.random(
+          0,
+          totalParentCheckboxes - 1
+        );
+        this.checkBoxInterested.eq(randomParentIndex).click();
+      });
+  };
+}
+module.exports = new contact();
